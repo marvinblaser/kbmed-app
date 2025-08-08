888a4086aecd44c07c077aa5763d718c068b0c52
marvinblaser <marvinblaser.pro@gmail.com>
2025-08-08 14:12:16 +0200

    normal

--- assets/js/components.js
+++ assets/js/components.js
@@ -18,17 +18,15 @@ export function loadComponent(id, file) {
         const sidebar = document.querySelector(".sidebar");
         const overlay = document.getElementById("sidebarOverlay");
 
-        if (burgerBtn && sidebar && overlay) {
-    burgerBtn.addEventListener("click", () => {
-      sidebar.classList.toggle("active");
-      overlay.classList.toggle("active");
-    });
+        burgerBtn.addEventListener("click", () => {
+          sidebar.classList.toggle("active");
+          overlay.classList.toggle("active");
+        });
 
         overlay.addEventListener("click", () => {
           sidebar.classList.remove("active");
           overlay.classList.remove("active");
         });
-    }
       }
     });
 }
